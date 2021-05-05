67b4a3140e9131d55e2ee322e372075029f69e96
Jonathan Z <izauritzb@gmail.com>
2021-05-05 17:39:02 -0400

    Only allow origin 'http://localhost:4200'.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,11 +8,14 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+// For local development running the client on port 4200. Edit if necessary, or remove param (or replace path with '*') to allow all.
+const origin = 'http://localhost:4200';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  app.enableCors();
+  app.enableCors({origin: origin});
   const port = process.env.PORT || 3333;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
